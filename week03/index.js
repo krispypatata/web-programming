1665b993ea3b12b991081d779548fd37a215500c
krispypatata <ksgabinete@up.edu.ph>
2024-09-09 17:23:16 +0800

    chore: ADD additional walkthroughs on basic javascript principles

--- week03/index.js
+++ week03/index.js
@@ -11,7 +11,7 @@ const arr1 = [];
 console.log(a, b, c);
 arr1.push(10);
 arr1.push(11);
-arr1.unshift(2);
+arr1.unshift(2);``
 
 
 
@@ -46,9 +46,59 @@ function letScope() {
         let b = 8;
         b = b + 1;
         console.log(b);
+        
     }
 
     console.log(b);
+
+    console.log(`typeof(${b}): ` + typeof(b));
 }
 
 letScope();
+
+
+// const typeChecker = null;
+var typeChecker; // undefined
+console.log(`typeof(${typeChecker}): ` + typeof(typeChecker));
+
+// Objects
+// Quotations are not required for keys
+
+const person = {
+    name: 'Tim Berners-Lee',
+    age: 66,
+    gender: 'male',
+    interests: ['physics', 'web'],
+}
+
+console.log(`typeof(${person}): ` + typeof(person));
+console.log(person);
+console.log(person.name);
+
+
+// Accessing items inside the object
+for (let i = 0; i < person.interests.length; i++) {
+    console.log(person.interests[i]);
+}
+
+// console.log(person["name"]);
+// Quotation marks are used to access elements with keys enclosed in quotation marks
+const animal = {
+    "number-of-legs": 4,
+    "fur-color": 'brown',
+}
+
+console.log(person["name"]);
+console.log(animal['fur-color']);
+console.log(animal["fur-color"]);
+
+animal.mammal = true;
+console.log(animal);
+
+delete animal.mammal;
+console.log(animal);
+
+const profile = ["Carl", 5, false, {color: "Green"}];
+
+console.log("\nArray");
+console.log(profile);
